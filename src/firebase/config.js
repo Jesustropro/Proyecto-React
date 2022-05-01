b790065f4ecf539695e88d3e2535d1d6d077bc9a
Jesustropro <jdmoraelpro1@gmail.com>
2022-05-01 12:06:42 -0300

    variables de entorno

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const APIKEY = process.env.APIKEY;
+const AUTHDOMAIN = process.env.AUTHDOMAIN;
+const PROJECTID = process.env.PROJECTID;
+const STORAGEBUCKET = process.env.STORAGEBUCKET;
+const MESSAGINSENDERID = process.env.MESSAGINSENDERID;
+const APPID = process.env.APPID;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: APIKEY,
-  authDomain: "proyecto-react-532f3.firebaseapp.com",
-  projectId: "proyecto-react-532f3",
-  storageBucket: "proyecto-react-532f3.appspot.com",
-  messagingSenderId: "504438247304",
-  appId: "1:504438247304:web:4509da58e05d9852aef43d",
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINSENDERID,
+  appId: APPID,
 };
 
 // Initialize Firebase
