9b31555ba29783e071c998f35eb4efccc57466b3
Jesustropro <jdmoraelpro1@gmail.com>
2022-05-01 20:18:21 -0300

    enviroment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-const APIKEY = process.env.APIKEY;
+const enviroment = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINSENDERID,
+  appId: process.env.APPID,
+};
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: APIKEY,
-  authDomain: "proyecto-react-532f3.firebaseapp.com",
-  projectId: "proyecto-react-532f3",
-  storageBucket: "proyecto-react-532f3.appspot.com",
-  messagingSenderId: "504438247304",
-  appId: "1:504438247304:web:4509da58e05d9852aef43d",
+  apiKey: enviroment.apiKey,
+  authDomain: enviroment.authDomain,
+  projectId: enviroment.projectId,
+  storageBucket: enviroment.storageBucket,
+  messagingSenderId: enviroment.messagingSenderId,
+  appId: enviroment.appId,
 };
 
 // Initialize Firebase
