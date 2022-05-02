eb847ef888ad5bfe138546497119df307d3824a0
Jesustropro <jdmoraelpro1@gmail.com>
2022-05-01 21:08:55 -0300

    fix env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,28 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-const {
-  APIKEY,
-  AUTHDOMAIN,
-  PROJECTID,
-  STORAGEBUCKET,
-  MESSAGINSENDERID,
-  APPID,
-} = process.env;
+const APIKEY = process.env.APIKEY;
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: APIKEY,
-  authDomain: AUTHDOMAIN,
-  projectId: PROJECTID,
-  storageBucket: STORAGEBUCKET,
-  messagingSenderId: MESSAGINSENDERID,
-  appId: APPID,
+  authDomain: "proyecto-react-532f3.firebaseapp.com",
+  projectId: "proyecto-react-532f3",
+  storageBucket: "proyecto-react-532f3.appspot.com",
+  messagingSenderId: "504438247304",
+  appId: "1:504438247304:web:4509da58e05d9852aef43d",
 };
 
-console.log(firebaseConfig, process.env.APIKEY, PROJECTID);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
